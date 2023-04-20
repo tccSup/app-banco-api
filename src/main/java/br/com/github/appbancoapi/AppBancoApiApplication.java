a0ca26a64363117c9c1f2303e4998e76317d206d
Vinicius Silva <viniciuspdasilva.dev@gmail.com>
2023-04-20 20:27:52 -0300

    Initial commit
    Initial commit

---
+++ src/main/java/br/com/github/appbancoapi/AppBancoApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.github.appbancoapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppBancoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppBancoApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/github/appbancoapi/AppBancoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.github.appbancoapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppBancoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
